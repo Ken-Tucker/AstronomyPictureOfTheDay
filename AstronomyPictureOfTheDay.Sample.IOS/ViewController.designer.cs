290606a8bcb1a230332b43f445713dbc76614946
Ken Tucker <vb2ae@live.com>
2018-12-24 17:38:51 -0500

    Added Ios sample

---
+++ AstronomyPictureOfTheDay.Sample.IOS/ViewController.designer.cs
@@ -0,0 +1,55 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace AstronomyPictureOfTheDay.Sample.IOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView imgPicOfDay { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblDate { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblDescription { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblTitle { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (imgPicOfDay != null) {
+                imgPicOfDay.Dispose ();
+                imgPicOfDay = null;
+            }
+
+            if (lblDate != null) {
+                lblDate.Dispose ();
+                lblDate = null;
+            }
+
+            if (lblDescription != null) {
+                lblDescription.Dispose ();
+                lblDescription = null;
+            }
+
+            if (lblTitle != null) {
+                lblTitle.Dispose ();
+                lblTitle = null;
+            }
+        }
+    }
+}
