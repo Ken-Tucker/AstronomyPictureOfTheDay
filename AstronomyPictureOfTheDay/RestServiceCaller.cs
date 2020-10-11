32b3c3abed8d108eb173dbbfdb2942235e5357d9
Ken Tucker <vb2ae@live.com>
2020-10-11 14:16:54 -0400

    add mars pictures

--- AstronomyPictureOfTheDay/RestServiceCaller.cs
+++ AstronomyPictureOfTheDay/RestServiceCaller.cs
@@ -20,5 +20,14 @@ namespace AstronomyPictureOfTheDay
             json = await client.GetStringAsync(url);
             return json;
         }
+
+        public async Task<string> GetMarsPictureJsonAsync(string rover, DateTime earthDate, string apiKey)
+        {
+            string url = $"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos?earth_date={earthDate.ToShortDateString()}&api_key={apiKey}";
+            string json = string.Empty;
+
+            json = await client.GetStringAsync(url);
+            return json;
+        }
     }
 }
