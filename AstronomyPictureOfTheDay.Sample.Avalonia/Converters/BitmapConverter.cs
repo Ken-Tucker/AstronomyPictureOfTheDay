ba6ba083c8511c2627c4524805648c17f43698a7
Ken Tucker <vb2ae@live.com>
2023-11-22 20:49:18 -0500

    create avalonia sample

---
+++ AstronomyPictureOfTheDay.Sample.Avalonia/Converters/BitmapConverter.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+
+namespace AstronomyPictureOfTheDay.Sample.Avalonia.Converters
+{
+    /// <summary>
+    /// <para>
+    /// Converts a string path to a bitmap asset.
+    /// </para>
+    /// </summary>
+    public class BitmapConverter : IValueConverter
+    {
+        public static BitmapConverter Instance = new BitmapConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+                return null;
+
+            if (value is string rawUri && targetType.IsAssignableFrom(typeof(Bitmap)))
+            {
+                using (var client = new WebClient())
+                {
+                    var content = client.DownloadData(rawUri);
+                    using (var stream = new MemoryStream(content))
+                    {
+                        return new Bitmap(stream);
+                    }
+                }
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ AstronomyPictureOfTheDay.Sample.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,101 @@
+﻿using AstronomyPictureOfTheDay.Entities;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading.Tasks;
+
+namespace AstronomyPictureOfTheDay.Sample.Avalonia.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            LoadMarsPictureCommand = new AsyncRelayCommand(LoadMarsPicture);
+            LoadPictureCommand = new AsyncRelayCommand(LoadPicture);
+            _picOfDay = "";
+            var getPictureTask = Task.Run(async () => await GetPictureOfTheDay());
+            getPictureTask.Wait();
+        }
+
+
+        private const string LoadImage = "Loading image";
+        public IAsyncRelayCommand LoadMarsPictureCommand { get; }
+
+        private async Task<bool> LoadMarsPicture()
+        {
+            Title = LoadImage;
+            await GetMarsPictureOfTheDay();
+            return true;
+        }
+
+        public IAsyncRelayCommand LoadPictureCommand { get; }
+
+        private async Task<bool> LoadPicture()
+        {
+            Title = LoadImage;
+            await GetPictureOfTheDay();
+            return true;
+        }
+
+        private string _title = LoadImage;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _picOfDay;
+
+        public string PictureOfDay
+        {
+            get
+            {
+                return _picOfDay;
+            }
+            set
+            {
+                _picOfDay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async Task<bool> GetPictureOfTheDay()
+        {
+            var apod = new NasaPictureOfTheDay();
+            PictureOfTheDayResponse response = null;
+            response = await apod.GetTodaysPictureAsync("DEMO_KEY");
+            if (response != null && response.Success)
+            {
+
+                Title = response.pictureOfTheDay.title;
+                PictureOfDay = response.pictureOfTheDay.url;
+            }
+
+            return true;
+        }
+
+        public async Task<bool> GetMarsPictureOfTheDay()
+        {
+            var marsPictureOfTheDay = new NasaPictureOfTheDay();
+            MarsPictureResponse response = null;
+
+            response = await marsPictureOfTheDay.GetMarsPictureAsync(RoverEnum.Curiosity, DateTime.Now.AddDays(-30), "DEMO_KEY");
+            if (response != null && response.Success)
+            {
+                Title = response.picturesFromMars.photos[0].camera.full_name;
+                PictureOfDay = response.picturesFromMars.photos[0].img_src;
+            }
+
+            return true;
+        }
+
+
+    }
+}
